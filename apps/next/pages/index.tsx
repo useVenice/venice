e05cd73673de6a881e6f4729166149346e14831f
Tony Xiao <tonyx.ca@gmail.com>
2022-08-22 18:27:10 +0200

    Adding new apps/next

---
+++ apps/next/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>LedgerSync Link Demo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        Hello world, this is nice
+      </div>
+    </>
+  )
+}
