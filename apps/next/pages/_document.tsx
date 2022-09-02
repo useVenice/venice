60d92fa9ced53be0199c5ddb7c0763bd150d3aa5
yenbekbay <ayan.yenb@gmail.com>
2022-09-02 17:31:33 +0600

    Update favicon on website

--- apps/next/pages/_document.tsx
+++ apps/next/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         </Head>
 
         <body>
