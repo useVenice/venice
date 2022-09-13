df95ab2dc2286cf360ee62997a4aeda59fa2fa08
Tony Xiao <tonyx.ca@gmail.com>
2022-09-13 15:29:46 +0200

    Persisting react query client for high performance

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -4,6 +4,8 @@ import {NextAdapter} from 'next-query-params'
 import type {AppProps} from 'next/app'
 import Head from 'next/head'
 import {QueryClient, QueryClientProvider} from 'react-query'
+import {createWebStoragePersistor} from 'react-query/createWebStoragePersistor-experimental'
+import {persistQueryClient} from 'react-query/persistQueryClient-experimental'
 import {QueryParamProvider} from 'use-query-params'
 
 import {ledgerSyncCommonConfig} from '@ledger-sync/app-config/commonConfig'
@@ -11,14 +13,31 @@ import {LSProvider} from '@ledger-sync/engine-frontend'
 
 import {PortalParamsProvider} from '../contexts/PortalParamsContext'
 
-const reactQueryClient = new QueryClient({
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 5 * 60 * 1000, // 5 mins by default, reduce refetching...
+      staleTime: 5 * 60 * 1000, // 5 mins by default, reduce refetching...
       refetchOnWindowFocus: false, // Too many requests for going between devTool and not... alternative is to change the stale time
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      // How do we configure it that the only time we "refetch" is when we cmd+r reload the window?
+      // We still want to stale-while-revalidate though and thus we persist the query cache.
     },
   },
 })
+
+if (typeof window !== 'undefined') {
+  const persistor = createWebStoragePersistor({storage: window.localStorage})
+  // persistor.removeClient() // Will clean up cache
+
+  void persistQueryClient({
+    queryClient,
+    persistor,
+    // Change this key if we change the format of the data to avoid crashes
+    // also we should clear any persisted data if userId ever changes
+    buster: undefined, // Should check against userId and version
+  })
+}
 
 export default function MyApp({Component, pageProps}: AppProps) {
   return (
@@ -29,10 +48,8 @@ export default function MyApp({Component, pageProps}: AppProps) {
       </Head>
 
       <QueryParamProvider adapter={NextAdapter}>
-        <QueryClientProvider client={reactQueryClient}>
-          <LSProvider
-            queryClient={reactQueryClient}
-            config={ledgerSyncCommonConfig}>
+        <QueryClientProvider client={queryClient}>
+          <LSProvider queryClient={queryClient} config={ledgerSyncCommonConfig}>
             <PortalParamsProvider>
               <Component {...pageProps} />
             </PortalParamsProvider>
