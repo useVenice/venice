1352f0f7b4e91f0cd3f300e4aa0c423646c1159c
Tony Xiao <tonyx.ca@gmail.com>
2022-09-16 03:13:42 +0200

    Preserve query when redirecting from root

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -2,17 +2,19 @@ import {useRouter} from 'next/router'
 import React from 'react'
 
 import {LSProvider} from '@ledger-sync/engine-frontend'
+import {stringifyQueryParams} from '@ledger-sync/util'
 
 export default function HomeScreen() {
   const router = useRouter()
   const {ledgerId, isAdmin} = LSProvider.useContext()
+  const query = stringifyQueryParams(router.query)
 
   React.useEffect(() => {
     if (isAdmin) {
-      void router.replace('/ledgers')
+      void router.push({pathname: '/ledgers', query})
     } else if (ledgerId) {
-      void router.replace(`/ledgers/${ledgerId}`)
+      void router.push({pathname: `/ledgers/${ledgerId}`, query})
     }
-  }, [isAdmin, ledgerId, router])
+  }, [isAdmin, ledgerId, query, router])
   return !isAdmin && !ledgerId ? <div>Unauthorized</div> : null
 }
