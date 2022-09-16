f09b53da84479f3aac48ca6b7c0b6ca2e62b434e
Tony Xiao <tonyx.ca@gmail.com>
2022-09-16 11:31:36 +0200

    Stop using useGetter which causes concurrency issues
    And resulting in stale ledgerId being used in header request

--- packages/@utils/util/debug-utils.ts
+++ packages/@utils/util/debug-utils.ts
@@ -2,4 +2,10 @@ export function debugMe<T>(input: T) {
   console.warn('[debugMe]', JSON.stringify(input, null, 2))
   return input
 }
-;(global as any).debugMe = debugMe
+;(globalThis as any).debugMe = debugMe
+
+declare global {
+  interface Window {
+    debugMe: typeof debugMe
+  }
+}
