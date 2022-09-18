20189cf7185b5ebbd6b486b8e9b247c67c540086
Tony Xiao <tonyx.ca@gmail.com>
2022-09-19 01:25:28 +0200

    Getting rid of dotenv JSON line hacks
    Now that we no longer prefer JSON values in env vars

--- apps/app-config/loadEnv.node.ts
+++ apps/app-config/loadEnv.node.ts
@@ -1,8 +1,6 @@
 // @next/env has been patched to use 'find-config' to load from parent
 import {loadEnvConfig} from '@next/env'
 
-import {safeJSONParse} from '@ledger-sync/util'
-
 /**
  * Vercel uses dotenv expand by default.. Really not great in case of a password with $
  * @see https://github.com/vercel/vercel/discussions/4391
@@ -10,28 +8,7 @@ import {safeJSONParse} from '@ledger-sync/util'
  */
 export function loadEnv() {
   const loaded = loadEnvConfig('./')
-  const envs = loaded.combinedEnv as Record<string, string>
-
-  // Workaround for https://github.com/motdotla/dotenv/issues/664
-  // This is in particular useful for YODLEE_CONFIG.production.proxy.cert attribute
-  // WARNING: YODLEE_CONFIG must be in the compact JSON format for this to work
-  // In multi-line mode the line break inside string gets confused with line break
-  // inside JSON values... We would need to either extend dotenv or extend vercel env pull
-  // to fix it. Shouldn't be a problem during actual deploys though
-  for (const [k, v] of Object.entries(envs ?? {})) {
-    if (safeJSONParse(v) === undefined) {
-      const jsonStr = v.split('\n').join('\\n')
-      if (safeJSONParse(jsonStr) !== undefined) {
-        // console.log('Hacking json support for key =', k)
-        envs[k] = jsonStr
-        if (v === process.env[k]) {
-          // Do not overwrite
-          process.env[k] = jsonStr
-        }
-      }
-    }
-  }
-  return envs
+  return loaded.combinedEnv as Record<string, string>
 }
 
 // console.log('[DEBUG] parsed dotenv', dotEnvOut.parsed)
