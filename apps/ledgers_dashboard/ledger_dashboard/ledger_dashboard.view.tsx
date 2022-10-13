2c21e7ee70fa81bf97d5bad38e54fcb1db13eac5
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@gmail.com>
2022-10-13 14:10:09 +0700

    add other types of data for dashboard

--- apps/ledgers_dashboard/ledger_dashboard/ledger_dashboard.view.tsx
+++ apps/ledgers_dashboard/ledger_dashboard/ledger_dashboard.view.tsx
@@ -2,10 +2,17 @@ import {Stack, Table, Title} from '@airplane/views'
 
 // Views documentation: https://docs.airplane.dev/views/getting-started
 const LedgerDashboard = () => (
-  <Stack>
-    <Title>Ledger dashboard</Title>
-    <Table title="Connections" task="list_connections" />
-  </Stack>
+  <>
+    <Stack>
+      <Title>Ledger dashboard</Title>
+      <Table title="Accounts" task="list_accounts" />
+      <Table title="Connections" task="list_connections" />
+      <Table title="Institutions" task="list_institutions" />
+      <Table title="Migrations" task="list_migrations" />
+      <Table title="Pipelines" task="list_pipelines" />
+      <Table title="Transactions" task="list_transactions" />
+    </Stack>
+  </>
 )
 
 export default LedgerDashboard
---
+++ apps/ledgers_dashboard/tasks/list_pipelines.sql
@@ -0,0 +1,3 @@
+-- Add your SQL queries here.
+-- See SQL documentation: https://docs.airplane.dev/creating-tasks/sql
+SELECT * from pipeline;
