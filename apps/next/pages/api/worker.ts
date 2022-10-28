c7f3eaee9421ca8c81f7120f5deb8966839663ba
Tony Xiao <tonyx.ca@gmail.com>
2022-10-28 19:16:03 +0200

    Update the vercel deploy command to use custom install for pnpm patch to work

--- apps/next/pages/api/worker.ts
+++ apps/next/pages/api/worker.ts
@@ -1,7 +1,5 @@
 import '@usevenice/app-config/register.node'
 
-import * as fs from 'node:fs'
-
 import type {NextApiHandler} from 'next'
 
 import {backendEnv} from '@usevenice/app-config/backendConfig'
@@ -26,7 +24,6 @@ export default R.identity<NextApiHandler>(async (req, res) => {
     res.status(401).send('WORKER_INVOCATION_SECRET required')
     return
   }
-  console.log('readDirSync', fs.readdirSync('./'))
   // 10 seconds for vercel personal plan and 60 seconds for team plan
   // So we default to 9 seconds (for idle anyways). This makes all forms of incremental sync super important.
 
