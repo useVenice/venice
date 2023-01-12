20aa648965a06aa1ed8b66cb8615432f86e152c8
Tony Xiao <tonyx.ca@gmail.com>
2023-01-12 21:13:32 +0100

    Combining data with explorer

--- apps/web/pages/v2/data-explorer.tsx
+++ apps/web/pages/v2/data-explorer.tsx
@@ -1,10 +1,22 @@
+import type {GridCell, GridColumn, Item} from '@glideapps/glide-data-grid'
+import {GridCellKind} from '@glideapps/glide-data-grid'
 import {Auth} from '@supabase/auth-ui-react'
+import dynamic from 'next/dynamic'
+import React from 'react'
 import {useSelect} from 'react-supabase'
 
-import {Container, EffectContainer, Loading} from '@usevenice/ui'
+import {Container} from '@usevenice/ui'
+import {produce} from '@usevenice/util'
 
 import {Layout} from '../../components/Layout'
 import {useRouterPlus} from '../../contexts/atoms'
+
+import '@glideapps/glide-data-grid/dist/index.css'
+
+const DataEditor = dynamic(
+  () => import('@glideapps/glide-data-grid').then((r) => r.DataEditor),
+  {ssr: false},
+)
 
 export default function DataExplorerScreen() {
   const {user} = Auth.useUser()
@@ -14,22 +26,46 @@ export default function DataExplorerScreen() {
   console.log('conns', connsRes.data)
 
   const [pipesRes] = useSelect('pipeline')
-  console.log('pipes', connsRes.data)
+  console.log('pipes', pipesRes.data)
 
   const [accountsRes] = useSelect('account')
   console.log('accounts', accountsRes.data)
 
-  if (!user) {
-    return (
-      <EffectContainer effect={() => void router.pushPathname('/v2/auth')}>
-        <Layout>
-          <Container className="flex-1">
-            <Loading />
-          </Container>
-        </Layout>
-      </EffectContainer>
-    )
+  // const [transactionsRes] = useSelect('transaction')
+  // console.log('transactions', transactionsRes.data)
+
+  // Grid columns may also provide icon, overlayIcon, menu, style, and theme overrides
+  const [columns, setColumns] = React.useState([
+    {id: 'id', title: 'Id', width: 100},
+    {id: 'provider_name', title: 'Provider Name', width: 100},
+  ] as GridColumn[])
+
+  // If fetching data is slow you can use the DataEditor ref to send updates for cells
+  // once data is loaded.
+
+  const items = accountsRes.data ?? []
+  function getData([colIdx, rowIdx]: Item): GridCell {
+    const col = columns[colIdx]!
+    const row = items[rowIdx]!
+    return {
+      kind: GridCellKind.Text,
+      data: row[col.id!] ?? '<empty>',
+      allowOverlay: false,
+      displayData: row[col.id!] ?? '<empty>',
+    }
   }
+
+  // if (!user) {
+  //   return (
+  //     <EffectContainer effect={() => void router.pushPathname('/v2/auth')}>
+  //       <Layout>
+  //         <Container className="flex-1">
+  //           <Loading />
+  //         </Container>
+  //       </Layout>
+  //     </EffectContainer>
+  //   )
+  // }
 
   return (
     <Layout
@@ -38,7 +74,7 @@ export default function DataExplorerScreen() {
         {label: 'Data explorer', href: '/v2/data-explorer'},
       ]}>
       <Container className="flex-1">
-        <span className="text-xs">You are logged in as {user.id}</span>
+        <span className="text-xs">You are logged in as {user?.id}</span>
         Connections
         <ul>
           {connsRes.data?.map((conn) => (
@@ -51,12 +87,23 @@ export default function DataExplorerScreen() {
             <li key={pipe.id}>{pipe.id}</li>
           ))}
         </ul>
-        Accounts
-        <ul>
-          {accountsRes.data?.map((account) => (
-            <li key={account.id}>{account.id}</li>
-          ))}
-        </ul>
+        <DataEditor
+          getCellContent={getData}
+          columns={columns}
+          rows={items.length}
+          onColumnResize={(col, newSize) => {
+            console.log('col resize', col, newSize)
+            setColumns((existing) =>
+              produce(existing, (draft) => {
+                const c = draft.find((c) => c.id === col.id)
+                if (c) {
+                  // @ts-expect-error
+                  c.width = newSize
+                }
+              }),
+            )
+          }}
+        />
       </Container>
     </Layout>
   )
