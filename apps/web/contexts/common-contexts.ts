501962e5b9e206c5cf430caab3288a2a5aaa53b5
Rob Phillips <rob@robphillips.me>
2023-01-20 16:03:15 -0500

    [Auth] Try to fix auto refresh token namespace
    Was going off of old docs

--- apps/web/contexts/common-contexts.ts
+++ apps/web/contexts/common-contexts.ts
@@ -5,7 +5,9 @@ export const supabase = createClient(
   'https://hhnxsazpojeczkeeifli.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhobnhzYXpwb2plY3prZWVpZmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyNjgwOTIsImV4cCI6MTk3NTg0NDA5Mn0.ZDmf1sjsr-UxW2bPgdj3uaqJNUSqkZh8vCB1phn3qqs',
   {
-    autoRefreshToken: true,
+    auth: {
+      autoRefreshToken: true,
+    },
   },
 )
 
