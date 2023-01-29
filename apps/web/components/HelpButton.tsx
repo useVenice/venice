add1070dd9490375907aab8616036da128ed3d39
Thanik Bhongbhibhat <email@thanik.me>
2023-01-29 21:56:28 +0700

    Make HelpButton text uppercase
    Co-authored-by: rob phillips <rob@robphillips.me>

--- apps/web/components/HelpButton.tsx
+++ apps/web/components/HelpButton.tsx
@@ -4,7 +4,7 @@ export function HelpButton() {
   return (
     <button className="flex h-8 shrink-0 items-center gap-1 rounded bg-venice-black-500 px-2 hover:opacity-90">
       <QuestionCircleIcon className="inline-flex h-4 w-4 fill-current text-offwhite" />
-      <span className="text-xs">Help</span>
+      <span className="text-xs uppercase">Help</span>
     </button>
   )
 }
