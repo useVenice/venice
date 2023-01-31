7d2303a5055169cf1edd8c22faf60c4f172e788a
Tony Xiao <tonyx.ca@gmail.com>
2023-01-31 15:05:22 +0800

    Fix healthcheck and upgrade to latest esbuild
    Needed to use `resolutions` for this due to esbuild-kit using outdated esbuild version

---
+++ bin/venice.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env tsx
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+require('@usevenice/cli/venice-cli').default.help().parse()
