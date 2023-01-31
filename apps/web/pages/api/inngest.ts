71b1ec581d77a33ee0b69ab13071b6bd378161c9
Tony Xiao <tonyx.ca@gmail.com>
2023-01-31 14:00:42 +0800

    Show landing page for inngest as long as we are not on prod

--- apps/web/pages/api/inngest.ts
+++ apps/web/pages/api/inngest.ts
@@ -4,6 +4,7 @@ import {serve} from 'inngest/next'
 import * as functions from '../../inngest/functions'
 
 export default serve(inngest.name, Object.values(functions), {
+  landingPage: process.env['VERCEL_ENV'] !== 'production',
   // serveHost: 'http://localhost:3010',
   // servePath: 'api/inngest'
 })
