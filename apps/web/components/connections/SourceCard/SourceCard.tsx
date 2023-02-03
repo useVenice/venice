2f0c4fea5d0df51d30ac978137e7b2a7fad1106c
Thanik Bhongbhibhat <email@thanik.me>
2023-02-03 15:54:02 +0700

    delete modal

--- apps/web/components/connections/SourceCard/SourceCard.tsx
+++ apps/web/components/connections/SourceCard/SourceCard.tsx
@@ -1,5 +1,5 @@
-import {DialogPrimitive as Dialog} from '@usevenice/ui'
 import {VeniceProvider} from '@usevenice/engine-frontend'
+import {DialogPrimitive as Dialog} from '@usevenice/ui'
 import clsx from 'clsx'
 import {formatDistanceToNowStrict} from 'date-fns'
 import Image from 'next/image'
@@ -15,7 +15,6 @@ import {
 } from '../../icons'
 import {ResourceCard} from '../../ResourceCard'
 import {ActionMenu, ActionMenuItem} from './ActionMenu'
-import {AddSourceDialog} from '../AddSourceDialog'
 
 export interface SourceCardProps {
   id: string
@@ -74,7 +73,7 @@ export function SourceCard(props: SourceCardProps) {
               onUpdateSuccess={() => setIsRenaming(false)}
             />
           ) : (
-            <span className="text-sm uppercase">{displayName}</span>
+            <span className="text-sm font-medium uppercase">{displayName}</span>
           )}
 
           <ActionMenu
@@ -102,17 +101,9 @@ export function SourceCard(props: SourceCardProps) {
               onClick={() => {
                 // need to close dropdown menu first
                 setActionMenuOpen(false)
+                // ...
                 setTimeout(() => setDeleteDialogOpen(true), 0)
               }}
-              // TODO: open delete confirmation modal
-              // onClick={async () => {
-              //   const result = await browserSupabase
-              //     .from('pipelines')
-              //     .delete()
-              //     .eq('id', id)
-
-              //   console.log(result)
-              // }}
             />
           </ActionMenu>
 
@@ -120,12 +111,20 @@ export function SourceCard(props: SourceCardProps) {
           <Dialog.Root
             open={isDeleteDialogOpen}
             onOpenChange={setDeleteDialogOpen}>
-            <DeleteSourceDialog />
+            <DeleteSourceDialog
+              resourceId=""
+              name="Wells Fargo"
+              institution={{
+                name: 'Wells Fargo',
+                logoUrl: '/wells-fargo-logo.svg',
+              }}
+              onCancel={() => setDeleteDialogOpen(false)}
+            />
           </Dialog.Root>
         </div>
         <div className="text-right">
           {status ? <ConnectionStatus status={status} /> : null}
-          <p className="text-xs text-venice-gray">
+          <p className="text-xs font-medium text-venice-gray">
             {lastSyncCompletedAt
               ? `Synced ${formatDistanceToNowStrict(
                   new Date(lastSyncCompletedAt),
@@ -204,20 +203,73 @@ function ConnectionStatus({status}: {status: ConnectionStatus}) {
   )
 }
 
-function DeleteSourceDialog() {
+interface DeleteSourceDialogProps {
+  institution?: {
+    name: string
+    logoUrl: string
+  }
+  name: string
+  onCancel: () => void
+  resourceId: string
+}
+
+function DeleteSourceDialog(props: DeleteSourceDialogProps) {
+  const {institution, name, onCancel, resourceId} = props
   return (
     <Dialog.Portal>
       <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
         <Dialog.Overlay className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm transition-opacity will-change-auto animate-in fade-in" />
-        <Dialog.Content className="fixed z-50 grid w-full gap-4 bg-venice-black-500 p-6 opacity-100 animate-in fade-in-70 focus:outline-none focus:ring-venice-black-300 sm:max-w-lg sm:rounded-lg">
-          <Dialog.Title>
+        <Dialog.Content className="fixed z-50 grid w-full gap-4 border border-venice-black-300 bg-venice-black-500 p-6 opacity-100 animate-in fade-in-70 focus:outline-none focus:ring-venice-black-300 sm:min-w-[35rem] sm:max-w-lg sm:rounded-lg">
+          <div className="mt-2 mb-3 flex items-center justify-center gap-2">
+            {institution?.logoUrl ? (
+              <Image
+                width={32}
+                height={32}
+                src={institution.logoUrl}
+                alt={`${institution.name} Logo`}
+              />
+            ) : (
+              <Image
+                width={32}
+                height={32}
+                src="/institution-placeholder.svg"
+                alt=""
+                aria-hidden="true"
+              />
+            )}
+            <span className="text-sm uppercase">{name}</span>
+          </div>
+          <Dialog.Title className="text-center text-venice-green">
             Are you sure you want to delete this connection?
           </Dialog.Title>
-          <Dialog.Description>
-            Deleting Wells Fargo will: stop syncing its data into Venice keep
-            all previously synced data safely in Venice delete this connection
-            with Wells Fargo
-          </Dialog.Description>
+          <div className="mx-auto max-w-[20rem] text-sm text-venice-gray">
+            <p className="pb-1">Deleting {name} will:</p>
+            <ul className="list-disc pl-4">
+              <li>stop syncing its data into Venice keep</li>
+              <li>all previously synced data safely in Venice</li>
+              <li>delete this connection with {name}</li>
+            </ul>
+          </div>
+          <div className="mt-12 flex justify-center gap-4">
+            <button
+              onClick={onCancel}
+              className="min-w-[6rem] rounded-lg px-3 py-2 text-sm text-offwhite ring-1 ring-inset ring-venice-black-400 transition-colors hover:bg-venice-black-400 focus:outline-none focus-visible:bg-venice-black-400">
+              Cancel
+            </button>
+            <button
+              onClick={async () => {
+                const result = await browserSupabase
+                  .from('pipelines')
+                  .delete()
+                  .eq('id', resourceId)
+
+                console.log(result)
+              }}
+              className="flex min-w-[6rem] items-center gap-2 rounded-lg bg-venice-red px-3 py-2 text-sm text-offwhite hover:bg-[#ac2039] focus:outline-none focus-visible:bg-[#ac2039]">
+              <DeleteIcon className="h-4 w-4 fill-current" />
+              <span>Delete</span>
+            </button>
+          </div>
         </Dialog.Content>
       </div>
     </Dialog.Portal>
