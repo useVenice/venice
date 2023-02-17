119d6b6bcdfc4aec3f8aa6c23e0c891ad0527d6c
Rob Phillips <rob@robphillips.me>
2023-02-17 10:26:37 -0500

    [DB] Add some right padding to match the left side

--- apps/web/components/api-access/DatabaseAccessCard.tsx
+++ apps/web/components/api-access/DatabaseAccessCard.tsx
@@ -19,7 +19,7 @@ export function DatabaseAccessCard(props: DatabaseAccessCardProps) {
         <li>Use your favorite SQL editor</li>
         <li>Write custom data analysis scripts</li>
       </ul>
-      <div className="flex gap-2 py-2">
+      <div className="flex gap-2 py-2 pr-7">
         <Input className="grow truncate" value={databaseUrl} readOnly />
         <CopyTextButton content={databaseUrl} />
       </div>
