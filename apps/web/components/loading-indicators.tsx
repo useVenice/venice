1fa5c2c4fcbd93727e2d5119c19130cab2469096
Thanik Bhongbhibhat <email@thanik.me>
2023-02-18 12:03:07 +0700

    Show loading overlay during pre-connect/post-connect integration

--- apps/web/components/loading-indicators.tsx
+++ apps/web/components/loading-indicators.tsx
@@ -1,9 +1,28 @@
+import {createPortal} from 'react-dom'
+
 // Full screen overlay with loading indicator
 export function LoadingIndicatorOverlay() {
   return (
     <div className="fixed inset-0 z-[9999]">
       <LoadingIndicator />
     </div>
+  )
+}
+
+interface LoadingIndicatorOverlayV2Props {
+  // the container must have "postiion: relative"
+  container?: Element | DocumentFragment
+}
+
+export function LoadingIndicatorOverlayV2(
+  props: LoadingIndicatorOverlayV2Props,
+) {
+  const {container = document.body} = props
+  return createPortal(
+    <div className="absolute inset-0 z-[9999] bg-venice-black/70">
+      <LoadingIndicator />
+    </div>,
+    container,
   )
 }
 
