e315a31a058d497dd61414cdbf4d939333e343da
Rob Phillips <rob@robphillips.me>
2023-02-18 22:29:42 -0500

    [Loading] tweak width

--- apps/web/components/loading-indicators.tsx
+++ apps/web/components/loading-indicators.tsx
@@ -30,7 +30,7 @@ export function LoadingIndicatorOverlayV2(
 export function LoadingIndicator() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="w-24">
+      <div className="w-40">
         <p className="text-center text-sm text-offwhite">Loading...</p>
         <progress className="progress progress-primary mt-4"></progress>
       </div>
