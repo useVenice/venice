25dad91443e26be8aea37318ac3a53137acf2dfd
Rob Phillips <rob@robphillips.me>
2023-02-25 14:12:49 -0500

    [SQL] Updated button icon to be email

--- apps/web/components/api-access/SQLAccessCard.tsx
+++ apps/web/components/api-access/SQLAccessCard.tsx
@@ -1,12 +1,12 @@
 import {Button, InstructionCard} from '@usevenice/ui'
-import {CodeIcon, DatabaseIcon} from '@usevenice/ui/icons'
+import {EmailIcon, DatabaseIcon} from '@usevenice/ui/icons'
 import Link from 'next/link'
 
 export function SQLAccessCard() {
   return (
     <InstructionCard icon={DatabaseIcon} title="Dedicated database package">
       <p className="text-venice-green">
-        Please reach out if you&asos;re interested in::
+        Please reach out if you&apos;re interested in::
       </p>
       <ul className="list-disc pl-4">
         <li>Dedicated Postgres database (or bring your own)</li>
@@ -14,9 +14,9 @@ export function SQLAccessCard() {
         <li>Raw SQL access (for backends, data analysis, etc.)</li>
       </ul>
       <div className="flex gap-2 py-2 pr-7">
-        <Button variant="primary" asChild className="gap-1">
+        <Button variant="primary" asChild className="gap-2">
           <Link href="mailto:hi@venice.is">
-            <CodeIcon className="h-4 w-4 fill-current text-offwhite" />
+            <EmailIcon className="h-4 w-4 fill-current text-offwhite" />
             Contact Us
           </Link>
         </Button>
