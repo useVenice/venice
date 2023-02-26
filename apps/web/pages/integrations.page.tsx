f9b1b389420f9de650eb942c7240a64abf3fe39f
Rob Phillips <rob@robphillips.me>
2023-02-25 22:13:01 -0500

    [Integrations] Add settings button

--- apps/web/pages/integrations.page.tsx
+++ apps/web/pages/integrations.page.tsx
@@ -1,8 +1,10 @@
 import type {GetServerSideProps} from 'next'
 import {PageHeader} from '../components/PageHeader'
 import {PageLayout} from '../components/PageLayout'
-import {Card} from '@usevenice/ui'
+import {Button, Card} from '@usevenice/ui'
 import {PlaidLogo} from '@usevenice/ui/logos'
+import {SettingsIcon} from '@usevenice/ui/icons'
+import Link from 'next/link'
 
 // for server-side
 import {serverGetUser} from '../server'
@@ -29,11 +31,19 @@ export default function Page() {
     <PageLayout title="Integrations">
       <PageHeader title={['Integrations']} />
       <div className="p-10">
-        <Card className="grid h-[10rem] w-[8rem] justify-center gap-2 p-6">
-          <span className="text-center font-mono text-base text-offwhite">
+        <Card className="flex w-32 flex-col gap-4 p-2">
+          <span className="mt-2 text-center font-mono text-base text-offwhite">
             Plaid
           </span>
-          <PlaidLogo className="h-16 w-16" />
+          <div className="flex items-center justify-center">
+            <PlaidLogo className="h-16 w-16" />
+          </div>
+          <Button asChild className="mb-1">
+            <Link href="/integrations/plaid">
+              <SettingsIcon className="h-6 w-6 fill-current pr-2 text-gray" />
+              Settings
+            </Link>
+          </Button>
         </Card>
       </div>
     </PageLayout>
