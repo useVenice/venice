f637d1c9bc2fc3edbd99f705c9fd320d0a26fbe7
Rob Phillips <rob@robphillips.me>
2023-02-25 23:59:02 -0500

    [Integrations] Disable plaid settings for now

--- apps/web/pages/integrations.page.tsx
+++ apps/web/pages/integrations.page.tsx
@@ -1,10 +1,8 @@
 import type {GetServerSideProps} from 'next'
 import {PageHeader} from '../components/PageHeader'
 import {PageLayout} from '../components/PageLayout'
-import {Button, Card} from '@usevenice/ui'
+import {Card} from '@usevenice/ui'
 import {PlaidLogo} from '@usevenice/ui/logos'
-import {SettingsIcon} from '@usevenice/ui/icons'
-import Link from 'next/link'
 
 // for server-side
 import {serverGetUser} from '../server'
@@ -35,15 +33,15 @@ export default function Page() {
           <span className="mt-2 text-center font-mono text-base text-offwhite">
             Plaid
           </span>
-          <div className="flex items-center justify-center">
+          <div className="mb-6 flex items-center justify-center">
             <PlaidLogo className="h-16 w-16" />
           </div>
-          <Button asChild className="mb-1">
+          {/* <Button asChild className="mb-1">
             <Link href="/integrations/plaid">
               <SettingsIcon className="h-6 w-6 fill-current pr-2 text-gray" />
               Settings
             </Link>
-          </Button>
+          </Button> */}
         </Card>
       </div>
     </PageLayout>
