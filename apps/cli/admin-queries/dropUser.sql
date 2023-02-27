5219741ee41171442c20ccfc1c0caf7c345e3b83
Tony Xiao <tonyx.ca@gmail.com>
2023-02-27 17:17:51 +0800

    Backing up a couple functions we no longer use

--- apps/cli/admin-queries/dropUser.sql
+++
@@ -1,5 +0,0 @@
-REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "usr_deca735c-a043-4394-b12d-33e4b3717a83";
-REVOKE USAGE ON SCHEMA public FROM "usr_deca735c-a043-4394-b12d-33e4b3717a83";
-DROP USER "usr_deca735c-a043-4394-b12d-33e4b3717a83";
-UPDATE auth.users SET raw_user_meta_data = raw_user_meta_data - 'apiKey' WHERE id = ${userId};
-
