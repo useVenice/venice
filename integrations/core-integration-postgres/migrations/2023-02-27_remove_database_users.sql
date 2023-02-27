ff11e86d589ebdc20f1ff0f0d1774b844de2d11e
Tony Xiao <tonyx.ca@gmail.com>
2023-02-27 16:13:46 +0800

    Declare auth_user as record to make vanilla migration pass

--- integrations/core-integration-postgres/migrations/2023-02-27_remove_database_users.sql
+++ integrations/core-integration-postgres/migrations/2023-02-27_remove_database_users.sql
@@ -54,7 +54,7 @@ create or replace procedure auth.login_as_user (user_email text)
     language plpgsql
     as $$
 declare
-    auth_user auth.users;
+    auth_user record;
 begin
     select
         * into auth_user
@@ -62,13 +62,13 @@ begin
         auth.users
     where
         email = user_email;
-    execute format('set request.jwt.claim.sub=%L', (auth_user).id::text);
-    execute format('set request.jwt.claim.role=%I', (auth_user).role);
-    execute format('set request.jwt.claim.email=%L', (auth_user).email);
-    execute format('set request.jwt.claims=%L', json_strip_nulls(json_build_object('app_metadata', (auth_user).raw_app_meta_data))::text);
+    execute format('set request.jwt.claim.sub=%L', auth_user.id::text);
+    execute format('set request.jwt.claim.role=%I', auth_user.role);
+    execute format('set request.jwt.claim.email=%L', auth_user.email);
+    execute format('set request.jwt.claims=%L', json_strip_nulls(json_build_object('app_metadata', auth_user.raw_app_meta_data))::text);
 
-    raise notice '%', format( 'set role %I; -- logging in as %L (%L)', (auth_user).role, (auth_user).id, (auth_user).email);
-    execute format('set role %I', (auth_user).role);
+    raise notice '%', format( 'set role %I; -- logging in as %L (%L)', auth_user.role, auth_user.id, auth_user.email);
+    execute format('set role %I', auth_user.role);
 end
 $$;
 
