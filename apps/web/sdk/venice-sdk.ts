f773c15cde98a9dbfc054e5c4e60d9dde33f1953
Tony Xiao <tonyx.ca@gmail.com>
2023-03-01 10:51:50 +0800

    First attempt at sdk generation using openapi-typescript-fetch

---
+++ apps/web/sdk/venice-sdk.ts
@@ -0,0 +1,23 @@
+import type {paths} from './venice.gen'
+import {Fetcher} from 'openapi-typescript-fetch'
+
+const fetcher = Fetcher.for<paths>()
+fetcher.configure({
+  baseUrl: 'http://localhost:3000/api/rest',
+  init: {
+    headers: {
+      'x-token': process.env['VENICE_TOKEN'] ?? '',
+    },
+  },
+})
+
+async function main() {
+  const result = await fetcher.path('/account').method('get').create()({
+    // TODO: This should be taking params but it is not due to
+    // https://github.com/drwpow/openapi-typescript/issues/1040
+  })
+  console.log('result', result.data[0]?.name)
+  // result Plaid Gold Standard 0% Interest Checking (Plaid Checking) - 0000
+}
+
+main()
