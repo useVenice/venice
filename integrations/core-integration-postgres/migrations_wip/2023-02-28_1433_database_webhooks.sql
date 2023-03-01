68f420e4649a2d01a9a8710bad722ae9300c2508
Tony Xiao <tonyx.ca@gmail.com>
2023-03-01 12:55:16 +0800

    Success enable database webhooks on prod

--- integrations/core-integration-postgres/migrations_wip/2023-02-28_1433_database_webhooks.sql
+++ integrations/core-integration-postgres/migrations_wip/2023-02-28_1433_database_webhooks.sql
@@ -1,3 +1,8 @@
+-- create EXTENSION if not exists pg_net;
+-- Need to first go to the supabase dashboard to enable database webhooks
+-- before we can run the queries below
+-- @see https://share.cleanshot.com/CXtK7n1s
+
 -- This has to be run manually for now because the webhook urls cannot be parametrized during migrations :(
 
 -- [x] Dev
@@ -17,7 +22,7 @@ CREATE OR REPLACE TRIGGER "on-resource-write"
 	AFTER INSERT OR UPDATE OR DELETE ON public.resource FOR ROW
 	EXECUTE FUNCTION supabase_functions.http_request ('https://app-staging.venice.is/api/webhook/database', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
 
--- [ ] Production
+-- [x] Production
 CREATE OR REPLACE TRIGGER "on-users-write"
 	AFTER INSERT OR UPDATE OR DELETE ON auth.users FOR ROW
 	EXECUTE FUNCTION supabase_functions.http_request ('https://app.venice.is/api/webhook/database', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
