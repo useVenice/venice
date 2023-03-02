a7536b71bf964bb73c67bb85519be643d20dcc75
Tony Xiao <tonyx.ca@gmail.com>
2023-03-02 14:41:00 +0800

    Debugging and hack around inngest dev server 500 err
    For invalid signature. Thinks we are on prod somehow.
    Workaround: https://share.cleanshot.com/bG6jrvFB

--- apps/web/pages/api/inngest.page.ts
+++ apps/web/pages/api/inngest.page.ts
@@ -1,11 +1,24 @@
 import {inngest} from '@usevenice/engine-backend/events'
+import {withLog} from '@usevenice/util'
 
 import {serve} from 'inngest/next'
 import * as functions from '../../inngest/functions'
 
-export default serve(inngest.name, Object.values(functions), {
-  landingPage: process.env['VERCEL_ENV'] !== 'production',
-  logLevel: 'warn',
-  // serveHost: 'http://localhost:3010',
-  // servePath: 'api/inngest'
-})
+export default serve(
+  inngest.name,
+  Object.values(functions),
+  withLog('Starting inngest with', {
+    landingPage: process.env['VERCEL_ENV'] !== 'production',
+    logLevel: 'warn',
+    // Enforce dev env never hit production inngest
+    // https://discord.com/channels/842170679536517141/1080275520861782096/1080494988741324870
+    inngestRegisterUrl:
+      // For debugging...
+      process.env['INNGEST_REGISTER_URL'] ??
+      (process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8288/fn/register'
+        : undefined),
+    // For debugging, via e.g. 'http://localhost:3010' via mitmproxy
+    serveHost: process.env['INNGEST_SERVE_HOST'],
+  }),
+)
--- packages/util/debug-utils.ts
+++ packages/util/debug-utils.ts
@@ -1,13 +1,20 @@
+export function withLog<T>(prefix = '[withLog]', input: T) {
+  console.warn(prefix, input)
+  return input
+}
+
 export function debugMe<T>(input: T) {
   console.warn('[debugMe]', JSON.stringify(input, null, 2))
   return input
 }
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
 ;(globalThis as any).debugMe = debugMe
+;(globalThis as any).withLog = withLog
 
 declare global {
   interface Window {
     debugMe: typeof debugMe
+    withLog: typeof withLog
   }
 }
 
