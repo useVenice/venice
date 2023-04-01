c32a4c73924422f46c457cd83a3e7f9c8cf8b5d9
Tony Xiao <tonyx.ca@gmail.com>
2023-04-01 14:36:17 +0200

    Validate the token param on the connect page

--- apps/web/app/connect-wip/page.tsx
+++ apps/web/app/connect-wip/page.tsx
@@ -1,3 +1,26 @@
-export default function Connect() {
-  return <h1>Venice connect</h1>
+import '@usevenice/app-config/register.node'
+
+import {makeJwtClient} from '@usevenice/engine-backend'
+
+import {z} from '@usevenice/util'
+
+export const metadata = {
+  title: 'Venice Connect',
+}
+
+const jwtClient = makeJwtClient({
+  // app-config/backendConfig does not work inside server components, so we will have to use this for now...
+  secretOrPublicKey: process.env['JWT_SECRET_OR_PUBLIC_KEY']!, //  backendEnv.JWT_SECRET_OR_PUBLIC_KEY!,
+})
+
+/** https://beta.nextjs.org/docs/api-reference/file-conventions/page#searchparams-optional */
+export default function Connect({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>
+}) {
+  const token = z.string().parse(searchParams['token'])
+  const payload = jwtClient.verify(token)
+
+  return <h1>Venice connect {JSON.stringify(payload, null, 2)}</h1>
 }
