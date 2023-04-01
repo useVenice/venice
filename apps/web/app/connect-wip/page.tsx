5aca346979319e83da42ddded8b85a0ef867908d
Tony Xiao <tonyx.ca@gmail.com>
2023-04-01 14:54:12 +0200

    Async server component with veniceRouter "just works"
    Pretty magical to not have to deal with SSR quirks

--- apps/web/app/connect-wip/page.tsx
+++ apps/web/app/connect-wip/page.tsx
@@ -1,5 +1,8 @@
 import '@usevenice/app-config/register.node'
 
+import {backendEnv, veniceRouter} from '@usevenice/app-config/backendConfig'
+import type {UserId} from '@usevenice/cdk-core'
+
 import {makeJwtClient} from '@usevenice/engine-backend'
 
 import {z} from '@usevenice/util'
@@ -10,11 +13,11 @@ export const metadata = {
 
 const jwtClient = makeJwtClient({
   // app-config/backendConfig does not work inside server components, so we will have to use this for now...
-  secretOrPublicKey: process.env['JWT_SECRET_OR_PUBLIC_KEY']!, //  backendEnv.JWT_SECRET_OR_PUBLIC_KEY!,
+  secretOrPublicKey: backendEnv.JWT_SECRET_OR_PUBLIC_KEY,
 })
 
 /** https://beta.nextjs.org/docs/api-reference/file-conventions/page#searchparams-optional */
-export default function Connect({
+export default async function Connect({
   searchParams,
 }: {
   searchParams: Record<string, string | string[] | undefined>
@@ -22,5 +25,16 @@ export default function Connect({
   const token = z.string().parse(searchParams['token'])
   const payload = jwtClient.verify(token)
 
-  return <h1>Venice connect {JSON.stringify(payload, null, 2)}</h1>
+  const caller = veniceRouter.createCaller({userId: payload.sub as UserId})
+  const integrations = await caller.listIntegrations({})
+
+  // const caller = veniceRouter
+
+  return (
+    <div>
+      <h1>Venice connect </h1>
+      <pre>{JSON.stringify(payload, null, 2)}</pre>
+      <pre>{JSON.stringify(integrations, null, 2)}</pre>
+    </div>
+  )
 }
