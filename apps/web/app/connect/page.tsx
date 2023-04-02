f8d6c8e1fbd00538a40cc87ce8989342bea3d9d8
Tony Xiao <tonyx.ca@gmail.com>
2023-04-02 02:27:10 +0200

    Fix vercel build

--- apps/web/app/connect/page.tsx
+++ apps/web/app/connect/page.tsx
@@ -19,11 +19,6 @@ export const metadata = {
   title: 'Venice Connect',
 }
 
-const jwtClient = makeJwtClient({
-  // app-config/backendConfig does not work inside server components, so we will have to use this for now...
-  secretOrPublicKey: backendEnv.JWT_SECRET_OR_PUBLIC_KEY,
-})
-
 /** https://beta.nextjs.org/docs/api-reference/file-conventions/page#searchparams-optional */
 export default async function ConnectPage({
   searchParams,
@@ -32,6 +27,12 @@ export default async function ConnectPage({
 }) {
   const token = z.string().parse(searchParams['token'])
 
+  // Need to create jwtClient in here vecause otherwise during vercel static pre-render
+  // it would crash...
+  const jwtClient = makeJwtClient({
+    // app-config/backendConfig does not work inside server components, so we will have to use this for now...
+    secretOrPublicKey: backendEnv.JWT_SECRET_OR_PUBLIC_KEY,
+  })
   const payload = zVeniceConnectJwtPayload.parse(jwtClient.verify(token))
   const userId = payload.sub
   const queryClient = new QueryClient()
