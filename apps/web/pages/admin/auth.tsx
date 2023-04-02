abf5104b2fccd741953cad3b8f2e168dc4eb49ad
Tony Xiao <tonyx.ca@gmail.com>
2023-04-02 18:15:05 +0200

    Add button to refresh session for recently granted admins

--- apps/web/pages/admin/auth.tsx
+++ apps/web/pages/admin/auth.tsx
@@ -25,6 +25,9 @@ export default function AdminAuthScreen() {
   const logout = useMutation<void, Error>(async () => {
     await supabase.auth.signOut()
   })
+  const refreshSession = useMutation<void, Error>(async () => {
+    await supabase.auth.refreshSession()
+  })
 
   if (session && isAdmin) {
     return <RedirectTo url="/admin" />
@@ -35,21 +38,28 @@ export default function AdminAuthScreen() {
         <Container className="min-h-screen justify-center">
           <p>
             This portal is only accessible to admins. Please contact an existing
-            admin to grant you permission.
+            admin to grant you permission. If you were recently granted admin
+            access, press refresh session.
           </p>
-          <form
-            className="flex flex-col items-start gap-6"
-            onSubmit={(event) => {
-              event.preventDefault()
-              logout.mutate()
-            }}>
-            <div className="grid w-full max-w-[20rem] gap-3">
-              <Label>You are logged in as</Label>
-              <Input value={session?.user.email ?? session.user.id} readOnly />
-            </div>
+          <div className="grid w-full max-w-[20rem] gap-3">
+            <Label>You are logged in as</Label>
+            <Input value={session?.user.email ?? session.user.id} readOnly />
+          </div>
+          <div className="mt-4 flex flex-row gap-4 align-middle">
             <Button
               variant="primary"
               className="gap-2"
+              onClick={() => refreshSession.mutate()}
+              disabled={refreshSession.isLoading}>
+              {refreshSession.isLoading && (
+                <CircularProgress className="h-4 w-4 fill-offwhite text-offwhite/50" />
+              )}
+              Refresh session
+            </Button>
+            <Button
+              variant="default"
+              className="gap-2"
+              onClick={() => logout.mutate()}
               disabled={logout.isLoading}>
               {logout.isLoading ? (
                 <CircularProgress className="h-4 w-4 fill-offwhite text-offwhite/50" />
@@ -58,7 +68,7 @@ export default function AdminAuthScreen() {
               )}
               Log out
             </Button>
-          </form>
+          </div>
         </Container>
       </AdminPageLayout>
     )
