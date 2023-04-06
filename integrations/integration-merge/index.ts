7bf02e94f8e41c097c3eaf64c8a25ddc63104316
Tony Xiao <tonyx.ca@gmail.com>
2023-04-06 03:00:20 +0200

    Create integration-merge package and upgrade openapi-typescript

---
+++ integrations/integration-merge/index.ts
@@ -0,0 +1,4 @@
+// codegen:start {preset: barrel, include: "./{*.{ts,tsx},*/index.{ts,tsx}}", exclude: "./**/*.{d,spec,test,fixture,types,gen}.{ts,tsx}"}
+export * from './MergeClient'
+export * from './MergeProvider'
+// codegen:end
