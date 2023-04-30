98e2ada1c4c4bcdb8ba7f2d3e16cf6aa0dbcf7e9
Tony Xiao <tonyx.ca@gmail.com>
2023-05-01 01:33:17 +0200

    New section for available integrations

--- apps/web/app/(admin)/(authenticated)/integrations/page.tsx
+++ apps/web/app/(admin)/(authenticated)/integrations/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import {PROVIDERS} from '@usevenice/app-config/env'
 import {trpcReact} from '@usevenice/engine-frontend'
+import {Button, Card} from '@usevenice/ui/new-components'
 
 import {DataTable} from '@/components/DataTable'
 
@@ -10,12 +12,29 @@ export default function IntegrationsPage() {
   return (
     <div className="p-6">
       <h2 className="mb-4 text-2xl font-semibold tracking-tight">
-        Integrations
+        Configured integrations
       </h2>
+
       <DataTable
         isFetching={integrationsRes.isFetching}
         rows={integrationsRes.data ?? []}
       />
+      <h2 className="mb-4 text-2xl font-semibold tracking-tight">
+        Available integrations
+      </h2>
+      <div className="flex flex-wrap ">
+        {PROVIDERS.map((provider) => (
+          <Card
+            key={provider.name}
+            className="m-3 flex h-40 w-40 flex-col items-center p-2">
+            <div className="grow"></div> {/* Placeholder for image */}
+            <caption>{provider.name}</caption>
+            <Button className="mt-2" variant="default">
+              Add
+            </Button>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
