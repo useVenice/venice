9ff202759596314e1f4ff5e900cf33ffe0773182
Tony Xiao <tonyx.ca@gmail.com>
2023-05-02 22:19:44 +0200

    Coming soon for home and metric pages

--- apps/web/app/(admin)/(authenticated)/metrics/page.tsx
+++ apps/web/app/(admin)/(authenticated)/metrics/page.tsx
@@ -4,10 +4,6 @@ export default function ComingSoonPage() {
       <h2 className="mb-4 text-2xl font-semibold tracking-tight">
         Coming soon
       </h2>
-      <p>
-        A dashboard is coming soon. In the meantime check out entities section
-        and magic link page
-      </p>
     </div>
   )
 }
