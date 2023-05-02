1f9cd32fd6d46fe909762091a37077c068b44270
Tony Xiao <tonyx.ca@gmail.com>
2023-05-02 17:18:02 +0200

    Add back @tremor/react again
    And use it to display resources table

--- apps/web/app/(admin)/(authenticated)/resources/ResourcesPage.tsx
+++ apps/web/app/(admin)/(authenticated)/resources/ResourcesPage.tsx
@@ -1,9 +1,22 @@
 'use client'
 
+import {
+  Badge,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  Text,
+} from '@tremor/react'
+import {Loader2, Radio} from 'lucide-react'
+
 import {providerByName} from '@usevenice/app-config/providers'
+import type {RouterOutput} from '@usevenice/engine-backend'
 import {trpcReact, VeniceConnectButton} from '@usevenice/engine-frontend'
 
-import {DataTable} from '@/components/DataTable'
+type Resource = RouterOutput['listConnections'][number]
 
 export default function ResourcesPage() {
   const res = trpcReact.listConnections.useQuery({})
@@ -26,7 +39,50 @@ export default function ResourcesPage() {
         />
       </header>
       <p>Resources are created based on integration configurations</p>
-      <DataTable isFetching={res.isFetching} rows={res.data ?? []} />
+      {res.isFetching && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+      <ResourcesTable resources={res.data} />
     </div>
   )
 }
+
+// TODO: Abstract to introduce the definition of `column` so we don't need to manually
+// sync order between header and body...
+export const ResourcesTable = (props: {resources: Resource[]}) => (
+  // overflow-x-scroll Didn't work. Ideally we would like to always show a table with scrollbar
+  <Table className="mt-5">
+    <TableHead>
+      <TableRow>
+        <TableHeaderCell>Status</TableHeaderCell>
+        <TableHeaderCell>ID</TableHeaderCell>
+        <TableHeaderCell>Integration</TableHeaderCell>
+        <TableHeaderCell>Institution</TableHeaderCell>
+        <TableHeaderCell>End user Id</TableHeaderCell>
+        <TableHeaderCell>Settings</TableHeaderCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {props.resources.map((reso) => (
+        <TableRow key={reso.id}>
+          <TableCell>
+            <Badge color="emerald" icon={Radio}>
+              {reso.status}
+            </Badge>
+          </TableCell>
+          <TableCell>{reso.id}</TableCell>
+          <TableCell>
+            <Text>{reso.integrationId}</Text>
+          </TableCell>
+          <TableCell>
+            <Text>{reso.institutionId}</Text>
+          </TableCell>
+          <TableCell>
+            <Text>{reso.endUserId}</Text>
+          </TableCell>
+          <TableCell>
+            <Text>...</Text>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+)
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
 
     // Path to the tremor module
-    // './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
