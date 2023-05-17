f9ff95dbbf4e22445f4925f6579b8c330e4b722f
Tony Xiao <tonyx.ca@gmail.com>
2023-05-17 08:27:34 +0200

    Upgrade eslint plugin tailwindcss for native ts support

--- apps/web/tailwind.config.eslint.js
+++
@@ -1,4 +0,0 @@
-// See https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/228
-// @ts-ignore
-const jiti = require('jiti')(__filename)
-module.exports = jiti('./tailwind.config.ts').default
