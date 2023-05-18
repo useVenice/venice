a190aef966797ce81a76786ac901b275b44d8c63
Tony Xiao <tonyx.ca@gmail.com>
2023-05-18 12:46:34 +0200

    Prevent automatic redirect by authMiddleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,6 +1,7 @@
 import {authMiddleware} from '@clerk/nextjs'
 
-export default authMiddleware()
+// Disable redirects
+export default authMiddleware({publicRoutes: [/^(.*)/]})
 
 export const config = {
   /*
