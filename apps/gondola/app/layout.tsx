8a0925dc3d8006c2e1accf29061b6911b2e6339b
Tony Xiao <tonyx.ca@gmail.com>
2023-05-18 18:00:16 +0200

    Add nextjs-toploader

--- apps/gondola/app/layout.tsx
+++ apps/gondola/app/layout.tsx
@@ -1,5 +1,7 @@
 import './global.css'
 
+import NextTopLoader from 'nextjs-toploader'
+
 export const metadata = {
   title: 'Gondola, your financial DB',
   // icons: [{url: '/favicon.svg', type: 'image/svg+xml'}],
@@ -9,7 +11,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <head></head>
-      <body>{children}</body>
+      <body>
+        <NextTopLoader showSpinner={false} />
+        {children}
+      </body>
     </html>
   )
 }
