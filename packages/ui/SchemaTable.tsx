c0ad3948cf6a1654caf8965a539516509d2d5b86
Tony Xiao <tonyx.ca@gmail.com>
2023-05-19 09:39:22 +0200

    Switch over to shadcn table

--- packages/ui/SchemaTable.tsx
+++ packages/ui/SchemaTable.tsx
@@ -3,12 +3,13 @@
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
+  TableFooter,
   TableHead,
-  TableHeaderCell,
+  TableHeader,
   TableRow,
-  Text,
-} from '@tremor/react'
+} from './new-components'
 
 interface Column<T> {
   key: Extract<keyof T, string> | `$${string}`
@@ -19,34 +20,32 @@ interface Column<T> {
 export function SchemaTable<
   T extends {id: string},
   TC extends Array<Column<T>>,
->(props: {items: T[]; columns: TC}) {
+>(props: {items: T[]; columns: TC; caption?: string; footer?: string}) {
   return (
     // overflow-x-scroll Didn't work. Ideally we would like to always show a table with scrollbar
     <Table className="mt-5">
-      <TableHead>
+      {props.caption && <TableCaption>{props.caption}</TableCaption>}
+      <TableHeader>
         <TableRow>
           {props.columns.map((column) => (
-            <TableHeaderCell key={column.key}>
-              {column.title ?? column.key}
-            </TableHeaderCell>
+            <TableHead key={column.key}>{column.title ?? column.key}</TableHead>
           ))}
         </TableRow>
-      </TableHead>
+      </TableHeader>
       <TableBody>
         {props.items.map((item) => (
           <TableRow key={item.id}>
             {props.columns.map((column) => (
               <TableCell key={column.key}>
-                {column.render ? (
-                  column.render(item)
-                ) : (
-                  <Text>{`${item[column.key as keyof T] ?? ''}`}</Text>
-                )}
+                {column.render
+                  ? column.render(item)
+                  : `${item[column.key as keyof T] ?? ''}`}
               </TableCell>
             ))}
           </TableRow>
         ))}
       </TableBody>
+      {props.footer && <TableFooter>{props.footer}</TableFooter>}
     </Table>
   )
 }
