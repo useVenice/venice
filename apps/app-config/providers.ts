b74d2436ec92191c9bbffc051fd25ec5e8780604
Tony Xiao <tonyx.ca@gmail.com>
2023-05-26 22:30:28 +0200

    Generating merged integration lists
    Soon to deprecate providers.ts

--- apps/app-config/providers.ts
+++ apps/app-config/providers.ts
@@ -1,3 +1,5 @@
+// @deprecated. Soon to be fully replaced by integrations.merge.ts
+
 import {debugProvider} from '@usevenice/cdk-core'
 import {airtableProvider} from '@usevenice/core-integration-airtable'
 import {firebaseProvider} from '@usevenice/core-integration-firebase'
@@ -6,46 +8,32 @@ import {mongodbProvider} from '@usevenice/core-integration-mongodb'
 import {corePostgresProvider} from '@usevenice/core-integration-postgres'
 import {webhookProvider} from '@usevenice/core-integration-webhook'
 import {beancountProvider} from '@usevenice/integration-beancount'
-import {brexImpl} from '@usevenice/integration-brex'
 import {foreceiptProvider} from '@usevenice/integration-foreceipt'
-import {heronImpl} from '@usevenice/integration-heron'
 import {importProvider} from '@usevenice/integration-import'
 import {lunchmoneyProvider} from '@usevenice/integration-lunchmoney'
-import {mergeImpl} from '@usevenice/integration-merge'
 import {mootaProvider} from '@usevenice/integration-moota'
-import {oneBrickProvider} from '@usevenice/integration-onebrick'
 import {plaidProvider} from '@usevenice/integration-plaid'
 import {postgresProvider} from '@usevenice/integration-postgres'
 import {QBOProvider} from '@usevenice/integration-qbo'
 import {rampProvider} from '@usevenice/integration-ramp'
 import {saltedgeProvider} from '@usevenice/integration-saltedge'
 import {splitwiseProvider} from '@usevenice/integration-splitwise'
-import {stripeImpl} from '@usevenice/integration-stripe'
-import {tellerProvider} from '@usevenice/integration-teller'
 import {togglProvider} from '@usevenice/integration-toggl'
 import {venmoProvider} from '@usevenice/integration-venmo'
 import {wiseProvider} from '@usevenice/integration-wise'
-import {yodleeProvider} from '@usevenice/integration-yodlee'
+
+import {mergedIntegrations} from './integrations/integrations.merged'
 
 export {plaidProvider, fsProvider}
 
 export const DOCUMENTED_PROVIDERS = [
-  plaidProvider as unknown as typeof debugProvider,
-  mergeImpl as unknown as typeof debugProvider,
-  heronImpl as unknown as typeof debugProvider,
+  ...(Object.values(mergedIntegrations) as unknown as Array<
+    typeof debugProvider
+  >),
 ] as const
 
 export const PROVIDERS = [
   ...DOCUMENTED_PROVIDERS,
-
-  // Ledger
-  yodleeProvider as unknown as typeof debugProvider,
-  oneBrickProvider as unknown as typeof debugProvider,
-  tellerProvider as unknown as typeof debugProvider,
-  // New ones
-  stripeImpl as unknown as typeof debugProvider, // Hack for now..
-  brexImpl as unknown as typeof debugProvider, // Hack for now..
-
   // TODO: Migrate these over to the new paradigm
   debugProvider,
   fsProvider,
