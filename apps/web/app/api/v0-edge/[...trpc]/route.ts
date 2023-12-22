ec34b7e1f17b0848cb47e88f290d281d7949e03e
Tony Xiao <tonyx.ca@gmail.com>
2023-12-22 00:36:22 -0100

    fix: Work on vercel not just locally

--- apps/web/app/api/v0-edge/[...trpc]/route.ts
+++ apps/web/app/api/v0-edge/[...trpc]/route.ts
@@ -1,14 +1,17 @@
 import {modifyRequest, modifyResponse} from '@opensdks/fetch-links'
 
+export const runtime = 'edge'
+
 // Used to workaround issues where next.js node handler does not support incoming request with
 // transfer-encoding: chunked
 const handler = async (req: Request) => {
+  const body = await req.text()
+  console.log('[trpcOpenAPIHandler]', req.url, req.method, body)
   const res = await fetch(
     modifyRequest(req, {
       url: req.url.replace('/api/v0-edge', '/api/v0'),
       headers: (h) => h.delete('transfer-encoding'),
-      // @ts-expect-error https://github.com/nodejs/node/issues/46221
-      duplex: 'half',
+      body,
     }),
   )
   // console.log('[trpcOpenAPIHandler]', req.url, req.method, res.status)
