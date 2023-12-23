2cf0fde226451ed9b4fbefeb47608ac4ba6e44e4
Tony Xiao <tonyx.ca@gmail.com>
2023-12-23 22:23:06 -0100

    fix: Proxying request is not easy!
    Need explicit content-length header to void transfer-encoding: chunked
    and can only access body on POST / PUT / PATCH

--- apps/web/app/api/v0-edge/[...trpc]/route.ts
+++ apps/web/app/api/v0-edge/[...trpc]/route.ts
@@ -5,16 +5,24 @@ export const runtime = 'edge'
 // Used to workaround issues where next.js node handler does not support incoming request with
 // transfer-encoding: chunked
 const handler = async (req: Request) => {
-  const body = await req.text()
-  console.log('[v0-edge]', req.url, req.method, body)
-  const res = await fetch(
-    modifyRequest(req, {
-      url: req.url.replace('/api/v0-edge', '/api/v0'),
-      headers: (h) => h.delete('transfer-encoding'),
-      ...(body && {body}),
-    }),
-  )
-  // console.log('[trpcOpenAPIHandler]', req.url, req.method, res.status)
+  const body = ['POST', 'PUT', 'PATCH'].includes(req.method)
+    ? await req.blob()
+    : null
+  console.log('[v0-edge] Request', {url: req.url, method: req.method})
+
+  const modifiedReq = modifyRequest(req, {
+    url: req.url.replace('/api/v0-edge', '/api/v0'),
+    headers: (h) => {
+      if (body) {
+        h.set('content-length', body.size.toString())
+      }
+      h.delete('transfer-encoding')
+    },
+    body,
+  })
+  const res = await fetch(modifiedReq)
+
+  console.log('[v0-edge] response', res.status, res.headers)
 
   // otherwise crash if header exists...
   return modifyResponse(res, {headers: (h) => h.delete('x-middleware-rewrite')})
