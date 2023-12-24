8d837097374bebd8995415698028a5feac91d846
Tony Xiao <tonyx.ca@gmail.com>
2023-12-24 09:10:33 -0100

    feat: Removing OpenSDKs from Venice

--- docs/samples/sample.ts
+++ docs/samples/sample.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable unicorn/prefer-top-level-await */
-import {createClient} from '@usevenice/openapi-client'
-import {createSdk, createVeniceClient} from '@usevenice/sdk'
-import discordSdkDef from '@usevenice/sdk-discord'
-import openaiSdkDef from '@usevenice/sdk-openai'
-import slackSdkDef from '@usevenice/sdk-slack'
+import {initSDK} from '@opensdks/runtime'
+import discordSdkDef from '@opensdks/sdk-discord'
+import openaiSdkDef from '@opensdks/sdk-openai'
+import slackSdkDef from '@opensdks/sdk-slack'
+import {veniceSdkDef} from '@opensdks/sdk-venice'
 
-const discord = createSdk(discordSdkDef)
+const discord = initSDK(discordSdkDef, {
+  headers: {authorization: `Bearer ${process.env['DISCORD_TOKEN']}`},
+})
 
 void discord
   .GET('/channels/{channel_id}', {params: {path: {channel_id: ''}}})
@@ -13,32 +16,22 @@ void discord
     r.data.flags
   })
 
-const slack = createSdk(slackSdkDef)
+const slack = initSDK(slackSdkDef, {
+  headers: {token: process.env['SLACK_TOKEN']!},
+})
 void slack
   .GET('/admin.teams.admins.list', {
-    params: {query: {team_id: '', token: ''}},
+    params: {query: {team_id: ''}},
   })
   .then((r) => {
     r.data?.ok
   })
 
-const slack2 = createClient<(typeof slackSdkDef)['_types']['paths']>()
-void slack2
-  .GET('/admin.teams.admins.list', {
-    params: {query: {team_id: '', token: ''}},
-  })
-  .then((r) => {
-    r.data?.ok
-  })
-
-const openai = createSdk(openaiSdkDef)
-const apiKey = process.env['OPENAI_API_KEY']
+const openai = initSDK(openaiSdkDef, {
+  headers: {authorization: `Bearer ${process.env['OPENAI_API_KEY']}`},
+})
 void openai
   .POST('/chat/completions', {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    },
     body: {
       model: 'gpt-3.5-turbo',
       max_tokens: 1000,
@@ -50,7 +43,7 @@ void openai
     r.data.choices[0]?.message
   })
 
-const venice = createVeniceClient({})
+const venice = initSDK(veniceSdkDef, {headers: {}})
 
 void venice.GET('/core/resource').then((r) => {
   r.data[0]?.connectorConfigId
