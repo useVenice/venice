78c7c5c8a43cc29b53d0b77b8982d1e2d4dbc1da
Tony Xiao <tonyx.ca@gmail.com>
2024-03-14 17:46:49 -0700

    chore: empty com

--- apps/web/pages/api/inngest.ts
+++ apps/web/pages/api/inngest.ts
@@ -14,7 +14,6 @@ export default serve(
     client: inngest,
     functions: [
       ...Object.values(functions),
-
       // MARK: - Workaround for Inngest not having support for
       // multiple event triggers in a single function
       // @see https://discord.com/channels/842170679536517141/1214066130860118087/1214283616327180318
