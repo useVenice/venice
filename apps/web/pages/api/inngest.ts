579491df02edd5914ae9146c3db857762ebe48cd
Tony Xiao <tonyx.ca@gmail.com>
2024-03-14 18:45:16 -0700

    chore: only register inngest functions for events that we send webhooks for

--- apps/web/pages/api/inngest.ts
+++ apps/web/pages/api/inngest.ts
@@ -1,6 +1,9 @@
 import {serve} from 'inngest/next'
 import type {Events} from '@usevenice/engine-backend/events'
-import {eventMap, inngest} from '@usevenice/engine-backend/events'
+import {
+  inngest,
+  outgoingWebhookEventMap,
+} from '@usevenice/engine-backend/events'
 import {withLog} from '@usevenice/util'
 import {sendWebhook} from '../../inngest/function-factory'
 import * as functions from '../../inngest/functions'
@@ -17,7 +20,7 @@ export default serve(
       // MARK: - Workaround for Inngest not having support for
       // multiple event triggers in a single function
       // @see https://discord.com/channels/842170679536517141/1214066130860118087/1214283616327180318
-      ...Object.keys(eventMap).map((name) =>
+      ...Object.keys(outgoingWebhookEventMap).map((name) =>
         inngest.createFunction(
           {id: `send-webhook/${name}`},
           {event: name as keyof Events},
