8f8bdee337dac630e88961d2cd08c675e15fd19a
Tony Xiao <tonyx.ca@gmail.com>
2024-03-21 21:04:38 -0700

    fix: add CRUD company test for twenty

--- connectors/connector-twenty/index.spec.ts
+++ connectors/connector-twenty/index.spec.ts
@@ -9,6 +9,47 @@ const twenty = initTwentySDK({
 })
 
 maybeTest('should return a list of companies', async () => {
-  const companies = await twenty.core.GET('/companies')
-  expect(companies.data.data?.companies).toBeTruthy()
+  const res = await twenty.core.GET('/companies')
+  expect(res.data.data?.companies).toBeTruthy()
+})
+
+maybeTest('CRUD company', async () => {
+  const create = await twenty.core.POST('/companies', {
+    body: {name: 'test company', domainName: 'test.com'},
+  })
+
+  // Twenty bug. @see https://revert-dev.slack.com/archives/C06KH6J8UTD/p1711079562721159
+  const company = create.data.data?.['createCompany' as 'company']
+  expect(company?.id).toBeTruthy()
+
+  const update = await twenty.core.PUT('/companies/{id}', {
+    body: {domainName: 'test2.com'},
+    params: {path: {id: company?.id!}},
+  })
+  expect(update.data.data?.['updateCompany' as 'company']?.domainName).toEqual(
+    'test2.com',
+  )
+
+  const get = await twenty.core.GET('/companies/{id}', {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    params: {path: {id: company?.id!}},
+  })
+
+  expect(get.data.data?.company?.name).toBe('test company')
+
+  const del = await twenty.core.DELETE('/companies/{id}', {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    params: {path: {id: company?.id!}},
+  })
+  expect(del.data.data?.['deleteCompany' as 'company']?.id).toEqual(company?.id)
+
+  // https://revert-dev.slack.com/archives/C06KH6J8UTD/p1711080124005239
+  // This does not work at the moment due to bug in Twenty that actually returns 200
+  // for not found...
+  // await expect(() =>
+  //   twenty.core.GET('/companies/{id}', {
+  //     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  //     params: {path: {id: company?.id!}},
+  //   }),
+  // ).rejects.toThrow('404')
 })
